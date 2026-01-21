02598ebd6f696f00ff853372bff5a730422ceeb3
Rafa38gh <siscaro.rafael96@gmail.com>
2026-01-21 12:00:50 -0300

    Frontend completo

--- src/app.js
+++ src/app.js
@@ -8,22 +8,29 @@ var logger = require('morgan');
 
 var registerRoutes = require('./routes');
 
-var app = express();
-
 const session = require('express-session');
+const expressLayouts = require('express-ejs-layouts');
 
-// Servir arquivos estáticos da pasta public
-app.use(express.static('public'));
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, '..', 'views'));
 app.set('view engine', 'ejs');
 
+// Setup de layouts
+app.use(expressLayouts);
+app.set('layout', 'layouts/main');
+
+// Static files
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Session
 app.use(session({
   name: 'gym.sid',
   secret: process.env.SESSION_SECRET,
@@ -36,8 +43,14 @@ app.use(session({
   }
 }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Global Locals
+app.use((req, res, next) => {
+  res.locals.user = req.session.user || null;
+  res.locals.title = 'GymApp';    // Mudar para o novo nome do App depois
+  next();
+});
 
+// Routes
 registerRoutes(app);
 
 // catch 404 and forward to error handler
